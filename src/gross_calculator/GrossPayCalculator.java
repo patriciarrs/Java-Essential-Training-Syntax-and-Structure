ef7c12f72f9295b1f9876d78defd3eced0e40bce
Patrícia Silva <prrs89@gmail.com>
2025-04-24 11:10:24 +0100

    [3 Variables] 3.1 Creating variables

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -2,6 +2,16 @@ package gross_calculator;
 
 public class GrossPayCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //1. Get the number of hours worked
+        int hours = 40;
+
+        //2. Get the hourly pay rate
+        double payRate = 25.50;
+
+        //3. Multiply hours and pay rate
+        double grossPay = hours * payRate;
+
+        //4. Display result
+        System.out.println("Gross pay: " + grossPay);
     }
 }
