363f705820eab59a723f2d83aee7060b6ddaf902
Patrícia Silva <prrs89@gmail.com>
2025-04-24 11:28:39 +0100

    [3 Variables] 3.5 Modifying variables

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,12 +1,22 @@
 package gross_calculator;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
     public static void main(String[] args) {
         //1. Get the number of hours worked
-        int hours = 40;
+        int hours = 0;
+        System.out.println("How many hours did you work?");
+
+        Scanner scanner = new Scanner(System.in);
+        hours = scanner.nextInt();
 
         //2. Get the hourly pay rate
-        double payRate = 25.50;
+        double payRate = 0;
+        System.out.println("What is your pay rate?");
+        payRate = scanner.nextDouble();
+
+        scanner.close();
 
         //3. Multiply hours and pay rate
         double grossPay = hours * payRate;
