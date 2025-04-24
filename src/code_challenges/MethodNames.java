f37b165711f76842bd859aeaa3f9b5a9fd99a4a5
Patrícia Silva <prrs89@gmail.com>
2025-04-24 13:56:40 +0100

    [2 Java Basics] 2.5 Code Challenge: Method names

---
+++ src/code_challenges/MethodNames.java
@@ -0,0 +1,36 @@
+package code_challenges;
+
+// Java code below
+
+/***************************************************
+ Write your answer here, and then test your code.
+ Your job is to identify the method's name.
+ ****************************************************/
+
+class Answer {
+
+    /***************************************************
+     Change these first two boolean values to control
+     whether you see the expected result and/or hints.
+     ****************************************************/
+    static boolean showExpectedResult = false;
+    static boolean showHints = false;
+
+    static String message = "default message";
+
+
+    public static void main(String[] args) {
+        // TODO: Write the method's name inside of the quotation marks below
+        String methodName = "main";
+        message = "The name of this method is " + methodName;
+
+        /*************************************************************
+         This is how your code will be called.
+         Your answer should contain the method's name.
+         *************************************************************/
+        String result = message;
+
+        System.out.println(result);
+    }
+
+}
