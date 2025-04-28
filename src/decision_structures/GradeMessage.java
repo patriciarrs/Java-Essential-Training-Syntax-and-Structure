2bcd42fc28175c64414cab6b9c539c8f98b9291b
Patrícia Silva <prrs89@gmail.com>
2025-04-28 10:18:06 +0100

    [4 Decision Structures] 4.4 Switch statements

---
+++ src/decision_structures/GradeMessage.java
@@ -0,0 +1,58 @@
+package decision_structures;
+
+import java.util.Scanner;
+
+/*
+ * SWITCH
+ * Have a user enter their letter grade, and using a switch statement,
+ * print out a message letting them know how they did.
+ */
+public class GradeMessage {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter your letter grade:");
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.next();
+        scanner.close();
+
+        String message;
+        switch (grade) {
+            case "A":
+                message = "Excellent job!";
+                break;
+            case "B":
+                message = "Great job!";
+                break;
+            case "C":
+                message = "Good job!";
+                break;
+            case "D":
+                message = "You need to work a bit harder";
+                break;
+            case "F":
+                message = "Uh oh!";
+                break;
+            default:
+                message = "Error. Invalid grade";
+                break;
+        }
+
+        System.out.println(message);
+
+        /* A fall-through occurs when we eliminate the break statement at the end of a case.
+        If the value of grade was A, it would execute case A.
+        However, because case A does not have a break statement, it would continue on to execute case B.
+        Sometimes, that's by design, as in the case of A and B here, where we may want to print the same message. */
+        /* switch (grade) {
+            case "A":
+            case "B":
+                message = "Great job!";
+                break;
+            case "C":
+                message = "Good job!";
+                break;
+        } */
+    }
+}
+
